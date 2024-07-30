2cfa2accae27cdf0d8a96ab1d3fb16813cf2d42d
SwastikSaha2002 <162354020+SwastikSaha2002@users.noreply.github.com>
2024-07-30 13:55:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+//detect clicks. 
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var drums = document.querySelectorAll(".drum");
+for(var i = 0; i < numberOfDrumButtons; i ++ ){
+    drums[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;   
+        sound(buttonInnerHTML);
+
+    });
+}
+//detect keyboard presses.
+document.addEventListener("keypress", function(event){
+    sound(event.key);
+})
+
+// plays sound.
+function sound(key){
+    switch (key){
+        case "w" :
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+    
+        case "a" :
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play()
+            break;
+
+        case "s" :
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d" :
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j" :
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "k" :
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        
+        case "l" :
+            var snare = new Audio("Sounds/snare.mp3");
+            snare.play();
+            break;
+        
+    }
+}
